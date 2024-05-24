a6b7604d88974c4b2d6a634741494c3f99aa9f9f
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-05-24 08:36:59 +0700

    feat: remove image file

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@
 import React, { use, useEffect, useState } from "react";
 import Breadcrumbs, { IBreadcumbItem } from "@/components/UI/Breadcumbs";
 import Link from "next/link";
-import { Avatar, Button, Input, Spacer, Tooltip, User } from "@nextui-org/react";
+import { Avatar, Badge, Button, Input, Spacer, Tooltip, User } from "@nextui-org/react";
 import { BiUser, BiPackage } from "react-icons/bi";
 import { RiCoupon3Line } from "react-icons/ri";
 import useCurrentUser from "@/hooks/useCurrentUser";
@@ -11,6 +11,7 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "@/config/firebase";
 import useFirebaseAuthStore from "@/hooks/useFirebaseAuthStore";
 import { toast } from "sonner";
+import { AiOutlineClose } from "react-icons/ai";
 
 const breadcumbItems: IBreadcumbItem[] = [
     {
@@ -137,12 +138,16 @@ export default function ProfilePage(): React.ReactNode {
     };
 
     const clearUpdateState = (): void => {
+        setName(name);
+        removeImage();
+    };
+
+    const removeImage = (): void => {
         if (imageFile && imageURL) {
             URL.revokeObjectURL(imageURL);
+            setImageFile(null);
+            setImageURL(null);
         }
-        setName(name);
-        setImageURL(null);
-        setImageFile(null);
     };
 
     return (
@@ -150,7 +155,7 @@ export default function ProfilePage(): React.ReactNode {
             <h3 className="text-xl font-bold mb-5">Hồ sơ của tôi</h3>
 
             <div className="grid grid-cols-12 gap-5">
-                <div className="col-span-6">
+                <div className="col-span-7">
                     <div>
                         <Input
                             isDisabled
@@ -183,17 +188,25 @@ export default function ProfilePage(): React.ReactNode {
                         </Button>
                     </div>
                 </div>
-                <div className="col-span-6">
+                <div className="col-span-5">
                     <div>
-                        <label htmlFor="avatar">
-                            <Tooltip content="Cập nhật ảnh đại diện" placement="bottom" color="foreground">
+                        <Tooltip closeDelay={0} content="Cập nhật ảnh đại diện" placement="bottom" color="foreground">
+                            <label htmlFor="avatar">
                                 <Avatar
                                     isBordered
                                     className="w-[300px] h-[300px] mx-auto cursor-pointer"
                                     src={imageURL || currentUser?.avatar || ""}
                                 />
-                            </Tooltip>
-                        </label>
+                            </label>
+                        </Tooltip>
+                        {imageFile && imageURL && (
+                            <div className="mt-5 flex justify-center">
+                                <Button onClick={() => removeImage()} color="danger" radius="full" variant="light">
+                                    Gỡ ảnh đại diện
+                                </Button>
+                            </div>
+                        )}
+
                         <input
                             onChange={handlePreviewAvatar}
                             hidden
